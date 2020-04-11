3f037ae67acdb6f8b7d71793377ba753083c34b5
Sarbik Betal <sarbikb55@gmail.com>
2020-04-11 20:26:47 +0530

    code cleanup

--- drums.js
+++ drums.js
@@ -1,37 +1,48 @@
-function playSounds(e){
-    // console.log(e);
-    const audio = document.querySelector(`audio[class="${e.keyCode}"]`);
-     if (!audio) return;
-     audio.currentTime = 0;
-     audio.play();
-     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-     console.log(typeof(keyCode));
-     key.classList.add("playing");
- 
+const sounds = {
+    "65": "sounds/clap.wav",
+    "83": "sounds/hihat.wav",
+    "68": "sounds/kick.wav",
+    "70": "sounds/openhat.wav",
+    "71": "sounds/boom.wav",
+    "72": "sounds/ride.wav",
+    "74": "sounds/snare.wav",
+    "75": "sounds/tom.wav",
+    "76": "sounds/tink.wav"
+}
+
+function playSounds(e) {
+    let cc = e.keyCode.toString();
+    const audio = sounds[cc] ? new Audio(sounds[cc]) : null;
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add("playing");
+    setTimeout(() => {
+        key.classList.remove("playing");
+        clearTimeout();
+    }, 80);
 }
 function playSoundsOnClick(e) {
     e = e || window.event;
     var target = e.target || e.srcElement,
-    text = target.textContent || target.innerText;
+        text = target.textContent || target.innerText;
     keyco = text.charCodeAt(0);
     // console.log(keyco);
-    
-    const audio = document.querySelector(`audio[class="${keyco}"]`);
+
+    const audio = (keyco != 10) ? new Audio(sounds[keyco.toString()]) : null;
     // console.log(audio);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     const key = document.querySelector(`.key[data-key="${keyco}"]`);
     key.classList.add("playing");
-    
+    setTimeout(() => {
+        key.classList.remove("playing");
+        clearTimeout();
+    }, 80);
+
 }
-function removeTransition(e) {
-    // console.log(e);
-    if (e.propertyName !== "transform") return;
-    e.target.classList.remove("playing");
-}
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
 document.addEventListener("keydown", playSounds);
 document.addEventListener("click", playSoundsOnClick);
